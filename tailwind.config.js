785c679bf68af045a5db69b617b92f2dbdaa6805
Adam Marek <amarek4859@gmail.com>
2025-06-20 18:42:04 +0200

    tailwind fixes - safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    safelist: [
+        'text-primary',
+        'text-2xl',
+        'font-bold',
+        'bg-blue-300',
+        'p-4'
+    ],
     theme: {
         extend: {
             colors: {
