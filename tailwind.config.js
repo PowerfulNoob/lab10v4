aff82f73b44057960b63e46bcf93f468c1e7cc8c
Adam Marek <amarek4859@gmail.com>
2025-06-20 18:48:18 +0200

    more tailwind fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./src/style.css"],
     safelist: [
         'text-primary',
         'text-2xl',
